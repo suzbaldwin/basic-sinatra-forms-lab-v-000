803e16d6bdffac931cc263d5bb2cb76e64a8df17
suzy baldwin <gbaldwin24@comcast.net>
2018-03-14 14:54:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
+require_relative 'config/environment'
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
+  get "/newteam" do
+    erb :newteam
+  end
+  post "/team" do
+    "Team Name: #{params[:name]}"
+    "Coach: #{params[:coach]}"
+    "Point Guard: #{params[:pg]} "
+    "Shooting Guard: #{params[:sg]}"
+    "Power Forward: #{params[:pf]}"
+    "Small Forward: #{params[:sf]}"
+    "Center: #{params[:c]}"
+    erb :team
+  end
 
 end
